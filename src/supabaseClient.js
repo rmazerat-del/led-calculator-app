853b6e18a8a7a0b2b4fd6fdb1def4304ca23d338
Rémy <rmazerat@gmail.com>
2026-04-02 11:36:53 +0200

    use legacy anon key

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -2,5 +2,5 @@ import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
   'https://xbwkbnqbkpcchwdfpznt.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhid2tuYnFia3BjY2h3ZGZwem50Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM1MTMxNzQsImV4cCI6MjA1OTA4OTE3NH0.Ht0WnAyOvRDkE-OTtVkjE7Y8hJHFKS1NMmRL_x4NKZA'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhid2tibnFia3BjY2h3ZGZwem50Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUwNDY1MDMsImV4cCI6MjA5MDYyMjUwM30.0qknrGn_oAmADJQZxmN9og2BCRJQWVM4djWzv-FSQLk'
 )
